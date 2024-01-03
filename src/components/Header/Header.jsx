7079eb3c863ab1d6ea4c7765bd34e596e8bc0403
benjnp <benjnp@gmail.com>
2024-01-04 00:48:20 +0800

    Added outside click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Header.css'
+import {BiMenuAltRight} from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler/build/OutsideClickHandler'
 
 export default function Header() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+  function getMenuStyles(menuOpened) {
+    if(document.documentElement.clientWidth <= 768) 
+      return {right: !isOpenMenu && "-100%"}
+  }
   return (
     <div className="h-wrapper">
         <div className="h-container flexCenter fullWidth">
             <img src="./realzTP.png" alt="logo" width="100" />
-            <div className="flexCenter h-menu">
+            <OutsideClickHandler onOutsideClick={() => setIsOpenMenu(false)}>
+            <div className="flexCenter h-menu" style={getMenuStyles(isOpenMenu)}>
                 <a href="">Residences</a>
                 <a href="">Our Values</a>
                 <a href="">Contact Us</a>
@@ -15,7 +24,12 @@ export default function Header() {
                     Contact Us
                 </button>
             </div>
+            </OutsideClickHandler>
+            <div className="menu-icon" onClick={() => setIsOpenMenu((prev) => !prev)}>
+              <BiMenuAltRight size={30} />
+            </div>
         </div>
+        
     </div>
   )
 }
